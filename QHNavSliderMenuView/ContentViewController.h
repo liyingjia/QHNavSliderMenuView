fb8c75393cb707b13db0ca66ff105f9665918dc6
imqiuhang <imqiuhang@hotmail.com>
2015-08-13 10:28:11 +0800

    init

---
+++ QHNavSliderMenuView/ContentViewController.h
@@ -0,0 +1,14 @@
+//
+//  ContentViewController.h
+//  QHNavSliderMenuView
+//
+//  Created by imqiuhang on 15/8/12.
+//  Copyright (c) 2015年 imqiuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentViewController : UIViewController
+@property (nonatomic)int index;
+
+@end
